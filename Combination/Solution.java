a2e36d330f9a0f7e42ca98dff8797e06ff088718
변지욱/biglol/LG CNS(여수) <biglol@lgchem.com>
2020-08-31 18:13:49 +0900

    comb, perm, confusing road

---
+++ Combination/Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Integer[] problem = {0,1,2,3,4};
+        Integer[] answer = new Integer[4];
+        doCombination(problem, answer, 0, 0);
+    }
+
+    public static void doCombination(Integer[] full, Integer[] arr, int n, int idx){ // n 은 arr의 index, idx는 problem의 index
+        if(n==arr.length){
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i]);
+            }
+            System.out.println("");
+            return;
+        }
+        if(idx == full.length){
+            return;
+        }
+        arr[n] = full[idx];
+        doCombination(full, arr, n+1, idx+1);
+        doCombination(full, arr, n, idx+1);
+    }
+}
